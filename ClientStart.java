daf22e16bad684b044798733992a0ee1dd5f1c3b
neto2096 <emoralescarrasco9700@gmail.com>
2025-07-27 22:08:25 -0600

    simplified commments

--- ClientStart.java
+++ ClientStart.java
@@ -1,14 +1,12 @@
 // Name: Ernesto Morales Carrasco
 // Email: emoralescarras@cnm.edu
-// Assignment: Networking Assignment Part 1
+// Assignment: Networking Part 1
 // Purpose: Entry point to start the client application for the matrix client-server system
 
 public class ClientStart {
     // Main method to initialize and start the client
     public static void main(String[] args) {
-        // Create a new Client instance
         Client client = new Client();
-        // Start the client (initializes GUI and networking)
         client.start();
     }
 }
