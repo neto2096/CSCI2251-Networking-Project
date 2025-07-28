715a1c5cbeb32336596b6d88ab1e466f475289b6
neto2096 <emoralescarrasco9700@gmail.com>
2025-07-27 22:09:21 -0600

    created ServerStart.java

---
+++ ServerStart.java
@@ -0,0 +1,12 @@
+// Name: Ernesto Morales Carrasco
+// Email: emoralescarras@cnm.edu
+// Assignment: Networking Part 1
+// Purpose: Entry point to start the server application for receiving and displaying matrices
+
+public class ServerStart {
+    // Main method to initialize and start the server
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
+}
